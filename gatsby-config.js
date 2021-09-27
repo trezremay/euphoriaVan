0bd13cfb8986071f0e76dccc761228ff4c6bb0a4
trezremay <remythompson@gmail.com>
2021-09-27 18:02:21 +0100

    url change and gatsby change

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       summary: `who live in the UK, love to travel and are currently converting VW T5 into a campervan.`,
     },
     description: `Our journey through a camper conversion and living with it.`,
-    Url: `https://hardcore-einstein-31f9fd.netlify.app`,
+    Url: ``,
     social: {
       twitter: `van.euphoriaa`,
     },
